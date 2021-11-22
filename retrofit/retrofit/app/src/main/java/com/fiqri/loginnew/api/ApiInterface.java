171161ed783eb0ede9708c24091d7e8642dfec8b
Maulana Fiqri Nurul Fawaid <89387565+Fiqrimfnf@users.noreply.github.com>
2021-11-22 18:09:18 +0700

    Retro

---
+++ retrofit/retrofit/app/src/main/java/com/fiqri/loginnew/api/ApiInterface.java
@@ -0,0 +1,29 @@
+package com.fiqri.loginnew.api;
+
+import com.fiqri.loginnew.model.login.Login;
+import com.fiqri.loginnew.model.register.Register;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
+public interface ApiInterface {
+
+    @FormUrlEncoded
+    @POST("login.php")
+    Call<Login> loginResponse(
+            @Field("username") String username,
+            @Field("password") String password
+    );
+
+    @FormUrlEncoded
+    @POST("register.php")
+    Call<Register> registerResponse(
+            @Field("username") String username,
+            @Field("password") String password,
+            @Field("name") String name
+    );
+
+}
+
